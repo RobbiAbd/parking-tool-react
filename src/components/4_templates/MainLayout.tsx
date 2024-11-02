f46930d6981b0acbef0a05c718afaa35939402d7
robbiabd <robbyabd10@gmail.com>
2024-11-02 15:48:14 +0700

    feat: add marquee

--- src/components/4_templates/MainLayout.tsx
+++ src/components/4_templates/MainLayout.tsx
@@ -36,6 +36,7 @@ export default function MainLayout({children, className} : Props) {
                     {children}
                 </Card>
             </div>
+            <Marquee items={items} />
         </>
     )
 }
